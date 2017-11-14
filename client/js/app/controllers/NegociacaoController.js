061ab3b46deabc12b3e9c3a9b304013ec84625b9
Jean Caldeira <jean.oliveira@possible.com>
2017-11-14 17:16:34 -0200

    New changes

---
+++ client/js/app/controllers/NegociacaoController.js
@@ -0,0 +1,21 @@
+class NegociacaoController {
+   
+    constructor(){
+        let $ = document.querySelector.bind(document);
+        this._inputData = $("#data");
+        this._inputQuantidade = $("#quantidade");
+        this._inputValor = $("#valor");
+    }
+   
+    adiciona(event) {
+        event.preventDefault();
+        let data = new Date(this._inputData.value.replace(/-/g,','));
+      //  let negociacao = new Negociacao(
+        //       this._inputData.value,
+        //       this._inputaQuantidade.value,
+        //       this._inputValor.value
+       //    );
+
+        console.log(data);
+    }
+}
