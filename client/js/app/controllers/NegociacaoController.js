dc7610ac76aeace7994d053652b840f2f1cf6223
Jean Caldeira <jean.oliveira@possible.com>
2017-11-16 17:56:54 -0200

    Aula 05

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -5,17 +5,32 @@ class NegociacaoController {
         this._inputData = $("#data");
         this._inputQuantidade = $("#quantidade");
         this._inputValor = $("#valor");
+        this._listaNegociacoes = new ListaNegociacoes();
+        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
+
+        this._negociacoesView.update();
     }
    
     adiciona(event) {
         event.preventDefault();
-        let data = new Date(...this._inputData.value.split('-'));
-      //  let negociacao = new Negociacao(
-        //       this._inputData.value,
-        //       this._inputaQuantidade.value,
-        //       this._inputValor.value
-       //    );
+        
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._limpaFormulario();
 
-        console.log(data);
+    }
+
+    _criaNegociacao(){
+        return new Negociacao(
+            DateHelper.textoParaData(this._inputData.value),
+            this._inputQuantidade.value,
+            this._inputValor.value);
+    }
+    _limpaFormulario() {
+      
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+        this._inputData.focus();
     }
 }
+
